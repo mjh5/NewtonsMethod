3e0130bccd5a190450d1247c06508e2f9839b3e6
micha <micha@DESKTOP-EUIQ4IV>
2017-12-07 19:34:44 -0600

    Class variables and default constructor

--- src/NewtonsMethod.java
+++ src/NewtonsMethod.java
@@ -1,8 +1,32 @@
-
+/**
+ * 
+ * @author micha
+ * 
+ * This class will estimate the nth root of a number using Newtons Method recursively.
+ *
+ */
 public class NewtonsMethod {
 
+	/**
+	 * The power of the funtion used in the estimation.
+	 * f(x) = (x ^ power) - value;
+	 */
+	int power;
+	
+	/**
+	 * The value to estimating the root at.
+	 * f(x) = (x ^ power) - value
+	 */
+	int value;
+	
+	/**
+	 * Default constructor.
+	 */
 	public NewtonsMethod() {
-		// TODO Auto-generated constructor stub
+		power = 0;
+		value = 0;
 	}
+	
+	
 
 }
